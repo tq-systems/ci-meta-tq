b9e72bd6f3eb536ea951b6299c2c717848181102
Max Merchel <Max.Merchel@ew.tq-group.com>
2023-02-16 16:20:44 +0100

    treewide: switch to SPDX-License-Identifier
    Signed-off-by: Max Merchel <Max.Merchel@ew.tq-group.com>

--- ci/update.py
+++ ci/update.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0-or-later
 #
 # File:        update-modules.py
 # Copyright (C) 2019 TQ-Systems GmbH
@@ -6,24 +7,6 @@
 #
 # Description: utility script to update all submodules to HEAD of branch given
 #              in .gitmodules
-#
-# License:     GPLv2
-#
-###############################################################################
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ###############################################################################
 
